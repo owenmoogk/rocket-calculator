749a1fda5dd13a66f2a6daca4bbdbae0b9d35e92
owenmoogk <owenmoogk@gmail.com>
2021-05-26 19:02:54 -0400

    added error checking and github corner

--- js/calculations.js
+++ js/calculations.js
@@ -4,8 +4,13 @@ function calculate() {
 	thrust = parseFloat(document.getElementById("thrust").value)
 	burnTime = parseFloat(document.getElementById("burn-time").value)
 
-	console.log(mass, thrust, burnTime)
+	// if any are empty or not numbers
+	if (!(mass && thrust && burnTime)){
+		document.getElementById("error").innerText = "Please enter numbers in all three fields"
+		return
+	}
 
+	// performing calculations
 	gforce = mass * 9.8
 	fnet = thrust - gforce
 	accel = fnet / mass
@@ -15,6 +20,7 @@ function calculate() {
 	apogee = coastDist + burnDist
 	timeToApogee = (burnVel / 9.8) + burnTime
 	
+	// if the rocket does not have enough power
 	if (apogee <= 0){
 		document.getElementById("error").innerText = "Oh no! This rocket won't leave the ground!"
 		return
@@ -23,9 +29,10 @@ function calculate() {
 		document.getElementById("error").innerText = ""
 	}
 
+	// updating table of data
 	document.getElementById("gravitational-force").innerText = Number((gforce).toFixed(1))+"n"
 	document.getElementById("net-force").innerText = Number((fnet).toFixed(3))+"n"
-	document.getElementById("acceleration").innerText = Number((accel).toFixed(3))+"m/s/s"
+	document.getElementById("acceleration").innerText = Number((accel).toFixed(3))+"m/s^2"
 	document.getElementById("burn-velocity").innerText = Number((burnVel).toFixed(3))+"m/s"
 	document.getElementById("burn-distance").innerText = Number((burnDist).toFixed(3))+"m"
 	document.getElementById("coast-distance").innerText = Number((coastDist).toFixed(3))+"n"
